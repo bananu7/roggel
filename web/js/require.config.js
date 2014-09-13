2a57e1446114ed4cccd5da1b995fb8a5be1d9104
Radek Slupik <rightfold@gmail.com>
2014-09-13 22:05:46 +0200

    Add dependency on AngularJS

---
+++ web/js/require.config.js
@@ -0,0 +1,10 @@
+require.config({
+    paths: {
+        'angular': 'bower_components/angular/angular'
+    },
+    shim: {
+        'angular': {
+            exports: 'angular'
+        }
+    }
+});
