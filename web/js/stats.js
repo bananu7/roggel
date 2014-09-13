a7a14b7a6bf671874292ff8fda5fdec328783278
Radek Slupik <rightfold@gmail.com>
2014-09-13 22:59:41 +0200

    Extract statistics into a service

--- web/js/stats.js
+++ web/js/stats.js
@@ -1,14 +1,33 @@
 define(['js/roggel'], function(roggel) {
-    roggel.controller('Stats', ['$scope', function($scope) {
-        $scope.copperCount = 9003;
-        $scope.silverCount = 256;
-        $scope.goldCount = 200;
+    roggel.factory('stats', ['$rootScope', function($rootScope) {
+        var result = {
+            copperCount: 9003,
+            silverCount: 256,
+            goldCount: 200
+        };
 
         setInterval(function() {
-            $scope.copperCount += Math.round(Math.random() * 100);
-            $scope.silverCount += Math.round(Math.random() * 100);
-            $scope.goldCount += Math.round(Math.random() * 100);
-            $scope.$apply();
+            result.copperCount += Math.round(Math.random() * 100);
+            result.silverCount += Math.round(Math.random() * 100);
+            result.goldCount += Math.round(Math.random() * 100);
+            $rootScope.$apply();
         }, 500);
+
+        return result;
+    }]);
+
+    roggel.controller('Stats', ['$scope', 'stats', function($scope, stats) {
+        Object.defineProperty($scope, 'copperCount', {
+            enumerable: true,
+            get: function() { return stats.copperCount; }
+        });
+        Object.defineProperty($scope, 'silverCount', {
+            enumerable: true,
+            get: function() { return stats.silverCount; }
+        });
+        Object.defineProperty($scope, 'goldCount', {
+            enumerable: true,
+            get: function() { return stats.goldCount; }
+        });
     }]);
 });
