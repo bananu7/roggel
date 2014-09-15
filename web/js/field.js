ba679956e379aeb99af48ac9ebbe0a33094cad8c
Radek Slupik <rightfold@gmail.com>
2014-09-15 20:53:26 +0200

    Add gold, silver and copper mines

--- web/js/field.js
+++ web/js/field.js
@@ -6,20 +6,28 @@ define(['js/roggel'], function(roggel) {
                 $scope.field.push({
                     x: x,
                     y: y,
-                    color: '#3d9970'
+                    color: '#3d9970',
+                    scale: 1.0
                 });
             }
         }
 
-        for (var i = 0; i < 2; ++i) {
+        for (var i = 0; i < 18; ++i) {
             var current = { x: Math.round(Math.random() * 25), y: Math.round(Math.random() * 25) };
             for (var j = 0; j < 60; ++j) {
                 $scope.field.forEach(function(cell) {
                     if (cell.x === current.x && cell.y === current.y) {
-                        if (Math.random() < 0.1) {
-                            cell.color = 'gold';
+                        var value = Math.random();
+                        switch (true) {
+                        case value < 0.05: cell.color = 'gold'; break;
+                        case value < 0.15: cell.color = 'silver'; break;
+                        case value < 0.3: cell.color = '#b87333'; break;
+                        default: cell.color = '#999999'; break;
+                        }
+                        if (value < 0.3) {
+                            cell.scale = 0.1 + (Math.random() * 0.7);
                         } else {
-                            cell.color = '#aaaaaa';
+                            cell.scale = 1.0;
                         }
                     }
                 });
@@ -34,6 +42,7 @@ define(['js/roggel'], function(roggel) {
                 $scope.field.forEach(function(cell) {
                     if (cell.x === current.x && cell.y === current.y) {
                         cell.color = '#7fdbff';
+                        cell.scale = 1.0;
                     }
                 });
                 var dx = -1 + Math.round(Math.random() * 2);
